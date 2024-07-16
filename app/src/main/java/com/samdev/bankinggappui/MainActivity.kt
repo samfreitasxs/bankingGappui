ffe7803de1c65a76ae04be61751233fcaa20bfc7
samfreitasxs <samuel.sfdf@gmail.com>
2024-07-16 00:41:55 -0300

    implementando CardsSelection

--- app/src/main/java/com/samdev/bankinggappui/MainActivity.kt
+++ app/src/main/java/com/samdev/bankinggappui/MainActivity.kt
@@ -70,7 +70,7 @@ fun HomeScreen() {
 
 
         WalletSelection()
-//        CardsSelection()
+        CardsSelection()
             Spacer(modifier = Modifier.height(16.dp))
 //        FinanceSelection()
 //        CurrenciesSelection()
---
+++ app/src/main/java/com/samdev/bankinggappui/data/Card.kt
@@ -0,0 +1,12 @@
+package com.samdev.bankinggappui.data
+
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+data class Card(
+    val cardType: String,
+    val cardNumber: String,
+    val cardName: String,
+    val balance: Double,
+    val color: Brush
+)
